3e1e345cca6934f675b1d267ef3e06f9623ff45f
Mazurexen <matheusmazu@gmail.com>
2025-03-27 10:39:52 -0300

    dale

--- src/Contador.java
+++ src/Contador.java
@@ -2,37 +2,38 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Contador {
-	public static void main(String[] args) {
-		try (Scanner terminal = new Scanner(System.in)) {
-			System.out.println("Digite o primeiro parâmetro");
-			int parametroUm = terminal.nextInt();
-			System.out.println("Digite o segundo parâmetro");
-			int parametroDois = terminal.nextInt();
-			try {
-				contar(parametroUm, parametroDois);
-			} catch (InputMismatchException exception) {
-				System.out.println("Erro: " + exception.getMessage());
-			} catch (ParametrosInvalidosException exception) {
-				System.out.println("Erro: " + exception.getMessage());
-			}
-		}
-	}
+    public static void main(String[] args) {
+        try (Scanner terminal = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
 
-	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-		if (parametroUm < parametroDois) {
-			int totalOcorrencias = parametroDois - parametroUm;
-			System.out.println("Total de ocorrências: " + totalOcorrencias);
-			for (int number = 1; number <= totalOcorrencias; number++) {
-				System.out.println("Imprimindo o número: " + number);
-			}
-		} else {
-			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
-		}
-	}
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
 
-	public static class ParametrosInvalidosException extends Exception {
-		public ParametrosInvalidosException(String message) {
-			super(message);
-		}
-	}
+            contar(parametroUm, parametroDois);
+        } catch (InputMismatchException exception) {
+            System.out.println("Erro: Entrada inválida. Por favor, insira números inteiros.");
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("Erro: " + exception.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm < parametroDois) {
+            int totalOcorrencias = parametroDois - parametroUm;
+            System.out.println("Total de ocorrências: " + totalOcorrencias);
+
+            for (int number = 1; number <= totalOcorrencias; number++) {
+                System.out.println("Imprimindo o número: " + number);
+            }
+        } else {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+    }
+
+    public static class ParametrosInvalidosException extends Exception {
+        public ParametrosInvalidosException(String message) {
+            super(message);
+        }
+    }
 }
